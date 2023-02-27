efed0e284be5e022489f7cb34dbb6c27ac48646d
Cem Bıçakcı <cmbicakci@gmail.com>
2023-02-27 15:06:26 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, FlatList } from 'react-native';
+
+import products from './src/data/products'
+
+console.log(products)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+          </View>
+        )}
+        numColumns={2}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,4 +29,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    width: '50%',
+    padding: 1
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  }
 });
